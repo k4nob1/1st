c1f3ae70f0ee908abfa975475658658c6a41e7f0
k4nob1 <vinnok991@gmail.com>
2019-02-27 15:32:18 +0900

    do schisleniya

--- Projkt/numbers.h
+++ Projkt/numbers.h
@@ -3,9 +3,7 @@ using namespace std;
 
 string string_from_int(int n)
 {
-	
 	string answer;
-
 	switch(n)
 	{
 	case 0:
@@ -72,4 +70,171 @@ string string_from_int(int n)
 	}
 
 	return answer;
+}
+
+string int_to_sto(int n)
+{
+	string ans;
+	if(n<20)
+	{
+		ans = string_from_int(n);
+	}
+	else
+	if (n/10 == 2)
+	{
+		int a = n%10;
+		if (a==0) ans = "двадцать";
+		else
+		ans = "двадцать "+string_from_int(a);
+	}
+	else
+	if (n/10 == 3)
+	{
+		int a = n%10;
+		if (a==0) ans = "тридцать";
+		else
+		ans = "тридцать "+string_from_int(a);
+	}
+	else
+	if (n/10 == 4)
+	{
+		int a = n%10;
+		if (a==0) ans = "сорок";
+		else
+		ans = "сорок "+string_from_int(a);
+	}
+	else
+	if (n/10 == 5)
+	{
+		int a = n%10;
+		if (a==0) ans = "пятьдесят";
+		else
+		ans = "пятьдесят "+string_from_int(a);
+	}
+	else
+	if (n/10 == 6)
+	{
+		int a = n%10;
+		if (a==0) ans = "шестьдесят";
+		else
+		ans = "шестьдесят "+string_from_int(a);
+	}
+	else
+	if (n/10 == 7)
+	{
+		int a = n%10;
+		if (a==0) ans = "семьдесят";
+		else
+		ans = "семьдесят "+string_from_int(a);
+	}
+	else
+	if (n/10 == 8)
+	{
+		int a = n%10;
+		if (a==0) ans = "восемьдесят";
+		else
+		ans = "восемьдесят "+string_from_int(a);
+	}
+	else
+	if (n/10 == 9)
+	{
+		int a = n%10;
+		if (a==0) ans = "девяносто";
+		else
+		ans = "девяносто "+string_from_int(a);
+	}
+
+	return ans;
+}
+
+string kilo(int n)
+{
+	string ans;
+	int a;
+	if (n<100)
+	{ans = int_to_sto(n);}
+	if (n/100 == 1)
+	{
+		a=n%100;
+		if (a==0) ans="сто";
+		else
+		ans = "сто "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 2)
+	{
+		a=n%100;
+		if (a==0) ans="двести";
+		else
+		ans = "двести "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 3)
+	{
+		a=n%100;
+		if (a==0) ans="триста";
+		else
+		ans = "триста "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 4)
+	{
+		a=n%100;
+		if (a==0) ans="четыреста";
+		else
+		ans = "четыреста "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 5)
+	{
+		a=n%100;
+		if (a==0) ans="пятьсот";
+		else
+		ans = "пятьсот "+int_to_sto(a);
+	}
+	else	
+	if (n/100 == 6)
+	{
+		a=n%100;
+		if (a==0) ans="шестьсот";
+		else
+		ans = "шестьсот "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 7)
+	{
+		a=n%100;
+		if (a==0) ans="семьсот";
+		else
+		ans = "семьсот "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 8)
+	{
+		a=n%100;
+		if (a==0) ans="восемьсот";
+		else
+		ans = "восемьсот "+int_to_sto(a);
+	}
+	else
+	if (n/100 == 9)
+	{
+		a=n%100;
+		if (a==0) ans="девятьсот";
+		else
+		ans = "девятьсот "+int_to_sto(a);
+	}
+
+	return ans;
+}
+
+string sch(int n, string s1,string s2,string s3)
+{
+	string ans;
+	if (n%10==1) ans = kilo(n)+" "+s1;
+	else
+	if (n%10>1&&n%10<5) ans = kilo(n)+" "+s2;
+	else
+	ans = kilo(n)+" "+s3;
+	return ans;
 }
--- Projkt/tests.cpp
+++ Projkt/tests.cpp
@@ -5,31 +5,67 @@
 #include "numbers.h"
 
 BOOST_AUTO_TEST_SUITE( test_suite1 )
-
-BOOST_AUTO_TEST_CASE( test_case1 )
+//
+//BOOST_AUTO_TEST_CASE( test_case1 )
+//{
+//	BOOST_TEST( string_from_int(0) == string("ноль") );
+//	BOOST_TEST( string_from_int(1) == string("один") );
+//	BOOST_TEST( string_from_int(2) == string("два") );
+//	BOOST_TEST( string_from_int(3) == string("три") );
+//	BOOST_TEST( string_from_int(4) == string("четыре") );
+//	BOOST_TEST( string_from_int(5) == string("п€ть") );
+//	BOOST_TEST( string_from_int(6) == string("шесть") );
+//	BOOST_TEST( string_from_int(7) == string("семь") );
+//	BOOST_TEST( string_from_int(8) == string("восемь") );
+//	BOOST_TEST( string_from_int(9) == string("дев€ть") );
+//	BOOST_TEST( string_from_int(10) == string("дес€ть") );
+//	BOOST_TEST( string_from_int(11) == string("одинадцать") );
+//	BOOST_TEST( string_from_int(12) == string("двенадцать") );
+//	BOOST_TEST( string_from_int(13) == string("тринадцать") );
+//	BOOST_TEST( string_from_int(14) == string("четырнадцать") );
+//	BOOST_TEST( string_from_int(15) == string("п€тьнадцать") );
+//	BOOST_TEST( string_from_int(16) == string("шестьнадцать") );	
+//	BOOST_TEST( string_from_int(17) == string("семьнадцать") );
+//	BOOST_TEST( string_from_int(18) == string("восемьнадцать") );
+//	BOOST_TEST( string_from_int(19) == string("дев€тьнадцать") );
+//}
+//BOOST_AUTO_TEST_CASE( test_int_to_sto )
+//{
+//	BOOST_TEST( int_to_sto(6) == string("шесть") );
+//	BOOST_TEST( int_to_sto(7) == string("семь") );
+//	BOOST_TEST( int_to_sto(8) == string("восемь") );
+//	BOOST_TEST( int_to_sto(9) == string("дев€ть") );
+//	BOOST_TEST( int_to_sto(10) == string("дес€ть") );
+//	BOOST_TEST( int_to_sto(22) == string("двадцать два") );
+//	BOOST_TEST( int_to_sto(30) == string("тридцать") );
+//	BOOST_TEST( int_to_sto(99) == string("дев€носто дев€ть") );
+//	BOOST_TEST( int_to_sto(64) == string("шестьдес€т четыре") );
+//	BOOST_TEST( int_to_sto(86) == string("восемьдес€т шесть") );
+//
+//}
+//BOOST_AUTO_TEST_CASE( test_kilo )
+//{
+//	BOOST_TEST( kilo(6) == string("шесть") );
+//	BOOST_TEST( kilo(7) == string("семь") );
+//	BOOST_TEST( kilo(8) == string("восемь") );
+//	BOOST_TEST( kilo(9) == string("дев€ть") );
+//	BOOST_TEST( kilo(10) == string("дес€ть") );
+//	BOOST_TEST( kilo(22) == string("двадцать два") );
+//	BOOST_TEST( kilo(30) == string("тридцать") );
+//	BOOST_TEST( kilo(99) == string("дев€носто дев€ть") );
+//	BOOST_TEST( kilo(999) == string("дев€тьсот дев€носто дев€ть") );
+//	BOOST_TEST( kilo(886) == string("восемьсот восемьдес€т шесть") );
+//}
+BOOST_AUTO_TEST_CASE( test_sch )
 {
-	BOOST_TEST( string_from_int(0) == string("ноль") );
-	BOOST_TEST( string_from_int(1) == string("один") );
-	BOOST_TEST( string_from_int(2) == string("два") );
-	BOOST_TEST( string_from_int(3) == string("три") );
-	BOOST_TEST( string_from_int(4) == string("четыре") );
-	BOOST_TEST( string_from_int(5) == string("п€ть") );
-	BOOST_TEST( string_from_int(6) == string("шесть") );
-	BOOST_TEST( string_from_int(7) == string("семь") );
-	BOOST_TEST( string_from_int(8) == string("восемь") );
-	BOOST_TEST( string_from_int(9) == string("дев€ть") );
-	BOOST_TEST( string_from_int(10) == string("дес€ть") );
-	BOOST_TEST( string_from_int(11) == string("одинадцать") );
-	BOOST_TEST( string_from_int(12) == string("двенадцать") );
-	BOOST_TEST( string_from_int(13) == string("тринадцать") );
-	BOOST_TEST( string_from_int(14) == string("четырнадцать") );
-	BOOST_TEST( string_from_int(15) == string("п€тьнадцать") );
-	BOOST_TEST( string_from_int(16) == string("шестьнадцать") );	
-	BOOST_TEST( string_from_int(17) == string("семьнадцать") );
-	BOOST_TEST( string_from_int(18) == string("восемьнадцать") );
-	BOOST_TEST( string_from_int(19) == string("дев€тьнадцать") );
-
+	BOOST_TEST( sch(3,"рубль","рубл€","рублей") == string("три рубл€") );
+	BOOST_TEST( sch(1,"стул","стула","стульев") == string("один стул") );
+	BOOST_TEST( sch(6,"рубль","рубл€","рублей") == string("шесть рублей") );
+	BOOST_TEST( sch(101,"доллар","доллара","долларов") == string("сто один доллар") );
+	BOOST_TEST( sch(103,"гриб","гриба","грибов") == string("сто три гриба") );
+	BOOST_TEST( sch(999,"город","города","городов") == string("дев€тьсот дев€носто дев€ть городов") );
 }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 #endif
